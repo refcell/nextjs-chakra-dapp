db6478093f087b92844d57f275e7908b4ab72f1a
Andreas Bigger <abigger87@gmail.com>
2021-09-29 19:18:01 -0400

    dapp init NextjsChakraDapp

---
+++ src/NextjsChakraDapp.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.6;
+
+import "ds-test/test.sol";
+
+import "./NextjsChakraDapp.sol";
+
+contract NextjsChakraDappTest is DSTest {
+    NextjsChakraDapp dapp;
+
+    function setUp() public {
+        dapp = new NextjsChakraDapp();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
